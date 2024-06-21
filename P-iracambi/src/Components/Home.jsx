c75b682f163bd2f364c48577e137e221e948d4cd
Marlon <marzonhapacheco@gmail.com>
2024-06-21 13:16:00 -0300

    Home

--- P-iracambi/src/Components/Home.jsx
+++ P-iracambi/src/Components/Home.jsx
@@ -1,8 +1,11 @@
-
+import HomeContent from "./HomeContent"
+import styles from './homecontent.module.css'
 
 export default function Home() {
   return (
     <div>
+       <h1>Iracambi</h1>
+    <HomeContent/>
       
     </div>
   )
---
+++ P-iracambi/src/Components/HomeContent.jsx
@@ -0,0 +1,9 @@
+import styles from './homecontent.module.css'
+
+export default function HomeContent() {
+  return (
+    <div>
+       <p >SALVANDO FLORESTAS, TRANSFORMANDO VIDAS</p>
+    </div>
+  )
+}
